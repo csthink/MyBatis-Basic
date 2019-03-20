8108ebe7a9e608f3f4509abb2b818e7d90e9f360
杜明洋 <security.2009@live.cn>
2019-03-21 07:06:16 +0800

    设置字符集过滤器包含响应的过滤

--- Common/src/main/java/com/csthink/bbs/filter/CharsetEncodingFilter.java
+++ Common/src/main/java/com/csthink/bbs/filter/CharsetEncodingFilter.java
@@ -5,18 +5,21 @@ import java.io.IOException;
 
 public class CharsetEncodingFilter implements Filter {
 
-    private String encoding;
+    private String encoding = "UTF-8";
 
     public void init(FilterConfig filterConfig) throws ServletException {
-        encoding = filterConfig.getInitParameter("encoding");
+        if (null != filterConfig.getInitParameter("encoding")) {
+            encoding = filterConfig.getInitParameter("encoding");
+        }
     }
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding(encoding);
+        servletResponse.setCharacterEncoding(encoding);
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     public void destroy() {
-
+        encoding = null;
     }
 }
